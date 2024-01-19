cd8bf19d9510789030b4a87e512a5e3043cb5506
Riku Putkinen <putkinenriku@gmail.com>
2024-01-19 03:07:21 +0200

    4.6 done

--- osa4/blogilista/utils/list-helper.js
+++ osa4/blogilista/utils/list-helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 function dummy(blogs) {
   return 1
 }
@@ -14,8 +16,21 @@ function favoriteBlog(blogs) {
     : blogs.reduce((max, curr) => max.likes < curr.likes ? curr : max)
 }
 
+function mostBlogs(blogs) {
+  if (blogs.length === 0) return null
+  const counted = _.countBy(blogs, (blog) => blog.author)
+  
+  const authorList = []
+  for (const [key, value] of Object.entries(counted)) {
+    authorList.push({author: key, blogs: value})
+  }
+  
+  return authorList.reduce((max, curr) => max.blogs < curr.blogs ? curr : max)
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
