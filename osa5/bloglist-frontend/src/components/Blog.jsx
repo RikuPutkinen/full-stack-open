5fe059ec5367e504b579ed6aae86544f56a625a1
Riku Putkinen <putkinenriku@gmail.com>
2024-01-25 21:50:22 +0200

    5.7 done

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+
+  if (!showFull) { 
+    return (
+      <div style={blogStyle}>
+        <p>
+          {blog.title} {blog.author}
+          <button onClick={() => setShowFull(true)}>show</button>
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <button onClick={() => setShowFull(false)}>hide</button>
+      <p>{blog.title}</p>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes} <button>like</button></p>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+}
 
 export default Blog
