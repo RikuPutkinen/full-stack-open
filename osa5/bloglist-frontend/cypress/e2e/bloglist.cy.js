f79055e2cf9062d9dfb3a9b7003b4755680938a1
Riku Putkinen <putkinenriku@gmail.com>
2024-01-31 23:51:16 +0200

    5.18 done

--- osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      name: 'A',
+      username: 'AAA',
+      password: 'BBB'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('AAA')
+      cy.get('#password').type('BBB')
+      cy.get('#login-button').click()
+
+      cy.contains('A logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('EEE')
+      cy.get('#password').type('FFF')
+      cy.get('#login-button').click()
+
+      cy.contains('Wrong username or password')
+    })
   })
 })
--- osa5/bloglist-frontend/src/components/LoginForm.jsx
+++ osa5/bloglist-frontend/src/components/LoginForm.jsx
@@ -25,7 +25,7 @@ export default function LoginForm(
           value={password}
           onChange={({ target }) => setPassword(target.value)}/>
       </div>
-      <button type="submit">login</button>
+      <button id='login-button' type="submit">login</button>
     </form>
   )
 }
