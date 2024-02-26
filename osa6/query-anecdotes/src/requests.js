7cb52649843ab7ba311798913266bb8f1bac7a8a
Riku Putkinen <putkinenriku@gmail.com>
2024-02-26 11:53:09 +0200

    6.21 done

---
+++ osa6/query-anecdotes/src/requests.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export function getAnecdotes() {
+  return axios
+    .get(baseUrl)
+    .then(res => res.data)
+}
+
+export function createAnecdote(newAnecdote) {
+  return axios
+    .post(baseUrl, newAnecdote)
+    .then(res => res.data)
+}
